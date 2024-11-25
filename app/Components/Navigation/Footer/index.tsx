1aa41251781ab4d6a325dbab3ff661f167b4c70c
appiy08 <adarshpatidar.dev08@gmail.com>
2024-11-25 18:59:53 +0530

    Working on create home page

---
+++ app/Components/Navigation/Footer/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-2">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis aut
+        dolor quasi consequatur nisi deleniti. Nisi numquam, velit odit rem vel
+        quos facilis illo. Alias facilis sit libero ullam expedita tenetur et
+        nobis voluptatibus ratione pariatur. Cumque possimus in tempore
+        molestias unde atque rem reprehenderit deserunt libero fugit, odio
+        labore perspiciatis a quo minima corrupti numquam, perferendis
+        voluptatem id architecto? Animi voluptate vero illum doloribus a
+        recusandae praesentium beatae nam fugiat, corporis, quam excepturi,
+        natus quibusdam commodi enim ut eligendi blanditiis omnis? Laborum
+        deleniti ex, laboriosam consequatur cumque reprehenderit assumenda
+        impedit adipisci quae quia quisquam consequuntur ut dolor minima, vel
+        harum fugiat voluptate, eos qui quasi obcaecati! Ea fuga molestias
+        expedita aut rerum totam tempore soluta, eius obcaecati doloremque hic
+        laboriosam ducimus eligendi nam porro quam, nostrum architecto! Odit
+        tempora earum molestias vel. Quis, sed. Et veniam porro aperiam harum
+        ipsam hic corporis explicabo, veritatis pariatur tempore maiores aut
+        cupiditate dolorum optio blanditiis quo totam minima possimus numquam
+        perspiciatis. Sequi odit architecto sed eum sit ex nam accusantium atque
+        molestiae necessitatibus tempore et fugit, commodi doloribus, illo magni
+        aliquid quam veniam esse ipsa? Quaerat praesentium iure iste officiis
+        recusandae quae at laborum! Nemo voluptatum culpa deserunt voluptates
+        ipsam natus veniam blanditiis nam ipsum. Nihil harum expedita ab autem
+        sequi placeat dolore quod nesciunt perferendis vitae sunt debitis
+        cupiditate, repellendus suscipit veritatis repudiandae dolorem, pariatur
+        labore exercitationem id. Odio, aliquam quibusdam? Nesciunt, laboriosam
+        distinctio. Atque ad blanditiis voluptates repellendus, nisi earum dicta
+        iure quis delectus aliquam fuga? Beatae error vel illum vero. Repellat
+        aliquam porro odio?
+      </p>
+      <p className="row-start-3 flex gap-1 flex-wrap items-center justify-center">
+        &#169; Design & Developed by
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Appiy.
+        </a>
+        All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "./styles/globals.css";
+import Header from "./Components/Navigation/Header";
+import Footer from "./Components/Navigation/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   );
